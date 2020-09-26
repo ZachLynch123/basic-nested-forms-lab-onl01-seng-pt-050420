0a3ed409041e5267bebff00e1ee8ac462fc4cff0
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-26 20:51:59 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,7 +21,7 @@ class RecipesController < ApplicationController
   
   def recipe_params 
     params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
-    puts params[ingredients_attributes]
+    puts params[:ingredients_attributes]
     
   end
   
